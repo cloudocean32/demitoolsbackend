033beb6c084f86abc27338e2092849269b39589c
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-06-27 19:13:53 +0700

    Delete routes directory

--- routes/seo-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { seoCheck } = require('../controllers/seo-controller');
-
-router.post('/seo-checker', seoCheck);
-
-module.exports = router;
