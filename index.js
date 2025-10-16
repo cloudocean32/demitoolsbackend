8d78c8128fe46f80f19eb1af91ead5601fa95a48
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-10-16 13:38:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // Tambahkan ini jika belum ada
+const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors()); // Tambahkan ini jika belum ada
+app.use(cors());
 app.use(express.json());
 
 // Import Routes
 const seoRouter = require('./api/routes/seo-routes');
 const whoisRouter = require('./api/routes/whois-routes');
-const whoisHistoryRouter = require('./api/routes/whoxy-routes'); // MODIFIED: Import router baru
+const whoisHistoryRouter = require('./api/routes/whoxy-routes');
+const reputationRouter = require('./api/routes/reputation-routes'); // <-- TAMBAHAN BARU
 
 app.get('/', (req, res) => {
     res.send("Demitools Api Started!");
@@ -19,7 +20,8 @@ app.get('/', (req, res) => {
 // Use Routes
 app.use('/api', seoRouter);
 app.use('/api', whoisRouter);
-app.use('/api', whoisHistoryRouter); // MODIFIED: Daftarkan router baru
+app.use('/api', whoisHistoryRouter);
+app.use('/api', reputationRouter); // <-- TAMBAHAN BARU
 
 // Error Handling
 app.use((err, req, res, next) => {
