fb877f41d5b4b661ff36e85033e19916071826b9
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-10-16 13:58:30 +0700

    Update now reputation-controller.js
    Update now reputation-controller.js

--- api/controllers/reputation-controller.js
+++ api/controllers/reputation-controller.js
@@ -6,12 +6,10 @@ const getReputation = async (req, res, next) => {
     if (!domain) {
         return res.status(400).json({ success: false, error: 'Domain wajib diisi di dalam body request.' });
     }
-
     try {
         const reputationData = await apivoidService.checkDomainReputation(domain);
         res.json({ success: true, data: reputationData });
     } catch (error) {
-        // Teruskan error ke middleware error handling di index.js
         next(error);
     }
 };
