ec4c6ac19bd9f3647b0ba1deb1103d92f1faf7ba
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-10-16 14:13:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,28 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Import Routes
 const seoRouter = require('./api/routes/seo-routes');
 const whoisRouter = require('./api/routes/whois-routes');
 const whoisHistoryRouter = require('./api/routes/whoxy-routes');
-const reputationRouter = require('./api/routes/reputation-routes'); // <-- TAMBAHAN BARU
+const reputationRouter = require('./api/routes/reputation-routes');
 
 app.get('/', (req, res) => {
     res.send("Demitools Api Started!");
 })
 
-// Use Routes
 app.use('/api', seoRouter);
 app.use('/api', whoisRouter);
 app.use('/api', whoisHistoryRouter);
-app.use('/api', reputationRouter); // <-- TAMBAHAN BARU
+app.use('/api', reputationRouter);
 
-// Error Handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ success: false, error: err.message || 'Internal Server Error' });
 });
 
-// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
