265fcbcf5ed68415d019c73bd2f6cd1925525a2b
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-10-16 14:14:00 +0700

    Update reputation-routes.js

--- api/routes/reputation-routes.js
+++ api/routes/reputation-routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const reputationController = require('../controllers/reputation-controller');
 
-// Definisikan endpoint POST di /api/reputation
 router.post('/reputation', reputationController.getReputation);
 
 module.exports = router;
